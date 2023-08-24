7659fc18f5140e61d1bb5c48bf760e0ca1cd7e67
RyumaKatoh <ryuma012645@gmail.com>
2023-08-24 12:14:28 +0900

    ログイン、新規登録画面の背景編集

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -1,4 +1,5 @@
 class PracticesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
 
   def index
     @practices = Practice.includes(:user).order(created_at: :desc) 
