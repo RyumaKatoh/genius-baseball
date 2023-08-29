2b2278ed9e31221f13e80e7bd91efb261f3fb576
RyumaKatoh <ryuma012645@gmail.com>
2023-08-29 20:54:08 +0900

    コメント削除修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "practices#index"
 #   # マイページのルーティングにネスト
-#   resources :users, only: [:show, :edit, :update] do
-#     get :favorites, on: :collection
-#   end
+  resources :users, only: [:show] do
+    get :favorites, on: :collection
+  end
 
 # # 練習投稿表示のルーティングにネスト
   resources :practices do
