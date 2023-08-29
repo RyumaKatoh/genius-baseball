eb930c49cc1aea167a87a3ff57ed47c975dcbc7d
RyumaKatoh <ryuma012645@gmail.com>
2023-08-29 18:47:17 +0900

    非同期通信、お気に入りボタン装飾

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,25 @@
 class FavoritesController < ApplicationController
+  before_action :set_practice
+
   def create
     favorite = current_user.favorites.build(practice_id: params[:practice_id])
     favorite.save
-    redirect_to root_path
+    render partial: "favorites/favorite", locals: { practice: @practice }
+    # respond_to do |format|
+    #   format.js
+    # end
   end
 
   def destroy
     favorite = Favorite.find_by(practice_id: params[:practice_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to root_path
+    render partial: "favorites/favorite", locals: { practice: @practice }
+    # respond_to do |format|
+    #   format.js
+    # end
+  end
+
+  def set_practice
+    @practice = Practice.find(params[:practice_id])
   end
 end
