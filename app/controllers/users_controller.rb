993e109a962e19c8b2250cabad141de3ba3f4e79
RyumaKatoh <ryuma012645@gmail.com>
2023-08-30 16:09:34 +0900

    お気に入り一覧、ユーザー詳細に表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id]) 
     @practices = @user.practices
+
+    favorites = Favorite.where(user_id: @user.id).pluck(:practice_id)
+    @favorite_list = Practice.find(favorites)
   end
 end
